5d1f76e4f527dacdba4a6bedea15faf04dd55376
Khaltura <khalturin-yaroslav@mail.ru>
2025-02-19 09:06:29 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int, char**){
+    setlocale(LC_ALL,"Russian");
+    std::cout << "Разбудите Романыча плиз\n";
+   int a = 121;
+    int b = 234;
+    std::cout << "Ответ: "<< a + b;
+    
+}
